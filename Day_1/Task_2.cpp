1f5532c44333debbc10237ad0d51c5cea0cd228b
Yash Pal Singh <114682246+protonium71@users.noreply.github.com>
2023-01-02 17:22:22 +0530

    Update Task_2.cpp

--- Day_1/Task_2.cpp
+++ Day_1/Task_2.cpp
@@ -1,3 +1,5 @@
+// Task - Given an array of integers, your task is to find the smallest and second smallest element in the array. If smallest and second smallest do not exist, print -1.
+
 vector<int> minAnd2ndMin(int a[], int n) {
     if(n==1)return vector<int>{-1,-1};
     int i,min1=a[0],min2=a[0];
@@ -11,3 +13,6 @@ vector<int> minAnd2ndMin(int a[], int n) {
     if(min1==min2)return vector<int>{-1,-1};
     return vector<int>{min1,min2};
 }
+
+// Time Complexity - O(n)
+// Space Complexity -O(1)
