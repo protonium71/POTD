a37972c46b0586163d9c6a54b2b05fbdab0863f9
Yash Pal Singh <114682246+protonium71@users.noreply.github.com>
2023-01-03 16:05:52 +0530

    Create Task_1.cpp

---
+++ Day_3/Task_1.cpp
@@ -0,0 +1,21 @@
+// Task_1 - Given an integer numRows, return the first numRows of Pascal's triangle.
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> v(numRows);
+        int i,j;
+        v[0].push_back(1);
+        if(numRows==1)return v;
+        for(i=1;i<numRows;i++){
+            for(j=0;j<=i;j++){
+                if(j==0 || j==i)v[i].push_back(1);
+                else v[i].push_back(v[i-1][j]+v[i-1][j-1]);
+            }
+        }
+        return v;
+    }
+};
+
+// Time Complexity - O(n^2)
+// Space Complexity -O(1)
