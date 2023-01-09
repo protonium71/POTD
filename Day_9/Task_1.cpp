31757d69f2d023236117afa1e089b7d12adab928
Yash Pal Singh <114682246+protonium71@users.noreply.github.com>
2023-01-09 13:12:03 +0530

    Update Task_1.cpp

--- Day_9/Task_1.cpp
+++ Day_9/Task_1.cpp
@@ -2,10 +2,17 @@
    Problem Link - https://practice.geeksforgeeks.org/problems/power-of-2-1587115620/1
 */
 
+// First approach O(logn) time
 bool isPowerofTwo(long long n){
   if(n==0)return 0;
   return pow(2,floor(log2(n)))==n?1:0;
 }
 
-// Time Complexity - O(logn)
+// Second Approach O(1) time
+bool isPowerofTwo(long long n){
+  if(n==0)return 0;
+  return !(n&(n-1));
+}
+
+// Time Complexity - O(1)
 // Space Complexity - O(1)
